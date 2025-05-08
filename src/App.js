60d7828d3ff16416a1292ca73c0c052803ac80b6
richa-daripa <qwertybinary401@gmail.com>
2025-05-08 23:03:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [result, setResult] = useState("");
+  const [isCal, setisCal] = useState(false);
+
+  const calculate = () => {
+    try {
+      setResult(evaluate(result));
+      setisCal(true);
+    } catch (error) {
+      setResult("Error");
+      setisCal(true);
+    }
+  }
+  const handleClick = (value) => {
+
+    const op = ['+', '-', '*', '/', '.'];
+    if (op.includes(value) && op.includes(result.slice(-1))) {
+      return;
+    }
+
+    if (isCal) {
+      setResult("");
+      setisCal(false);
+    } 
+    setResult(prev => prev + value);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="container border p-4 bg-dark rounded shadow w-25">
+        <div className="text-center mb-3">
+          <input type="text" className="form-control text-end" value={result} placeholder="0" readOnly/>
+        </div>
+        <div className='d-flex justify-content-center '>
+         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult("")}>AC</button>
+         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult(prev=>prev.toString().slice(0,-1))}>DEL</button>
+         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult(prev=>prev/100)}>%</button>
+         <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("/")}>/</button>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("7")}>7</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("8")}>8</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("9")}>9</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("*")}>*</button>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("4")}>4</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("5")}>5</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("6")}>6</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("-")}>-</button>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("1")}>1</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("2")}>2</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("3")}>3</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("+")}>+</button>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick(".")}>.</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("0")}>0</button>
+          <button className="btn btn-secondary m-1 w-100" onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
