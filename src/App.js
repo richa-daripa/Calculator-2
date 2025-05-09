d7b6175aeb5d103278ef5555515ba49e9cc818c1
richa-daripa <qwertybinary401@gmail.com>
2025-05-09 19:25:46 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const handleClick = (value) => {
 
     const op = ['+', '-', '*', '/', '.'];
-    if (op.includes(value) && op.includes(result.slice(-1))) {
+    if (op.includes(value) && op.includes(result.toString().slice(-1))) {
       return;
     }
 
@@ -34,38 +34,38 @@ function App() {
 
   return (
     <div className="d-flex align-items-center justify-content-center vh-100">
-      <div className="container border p-4 bg-dark rounded shadow w-25">
+      <div className="container border p-4 bg-dark rounded-4 shadow" style={{width: "300px"}}>
         <div className="text-center mb-3">
-          <input type="text" className="form-control text-end" value={result} placeholder="0" readOnly/>
-        </div>
-        <div className='d-flex justify-content-center '>
-         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult("")}>AC</button>
-         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult(prev=>prev.toString().slice(0,-1))}>DEL</button>
-         <button className="btn btn-secondary m-1 w-100" onClick={()=>setResult(prev=>prev/100)}>%</button>
-         <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("/")}>/</button>
+          <input type="text" className="form-control text-end" style={{fontSize:"20px"}} value={result} placeholder="0" readOnly/>
         </div>
         <div className='d-flex justify-content-center'>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("7")}>7</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("8")}>8</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("9")}>9</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("*")}>*</button>
+         <button className="btn btn-secondary m-1 w-25" onClick={()=>setResult("")}>AC</button>
+         <button className="btn btn-secondary m-1 w-25" onClick={()=>setResult(prev=>prev.toString().slice(0,-1))}>DEL</button>
+         <button className="btn btn-secondary m-1 w-25" onClick={()=>setResult(prev=>prev/100)}>%</button>
+         <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("/")}>/</button>
         </div>
         <div className='d-flex justify-content-center'>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("4")}>4</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("5")}>5</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("6")}>6</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("-")}>-</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("7")}>7</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("8")}>8</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("9")}>9</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("*")}>*</button>
         </div>
         <div className='d-flex justify-content-center'>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("1")}>1</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("2")}>2</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("3")}>3</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("+")}>+</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("4")}>4</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("5")}>5</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("6")}>6</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("-")}>-</button>
         </div>
         <div className='d-flex justify-content-center'>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick(".")}>.</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={()=>handleClick("0")}>0</button>
-          <button className="btn btn-secondary m-1 w-100" onClick={calculate}>=</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("1")}>1</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("2")}>2</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("3")}>3</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("+")}>+</button>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick(".")}>.</button>
+          <button className="btn btn-secondary m-1 w-25" onClick={()=>handleClick("0")}>0</button>
+          <button className="btn btn-secondary m-1 w-50" onClick={calculate}>=</button>
         </div>
       </div>
     </div>
